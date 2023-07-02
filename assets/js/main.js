0c5b3462955063efd7dca5ba6804aab4f586d58f
freetimezp <kraftukrnet@ukr.net>
2023-07-02 23:44:14 +0300

    11. js-portfolio-21-web-dev. Scrollbar. Scroll up icon. Set active menu on scroll.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,33 @@
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    this.scrollY >= 350
+        ? scrollUp.classList.add("show-scroll")
+        : scrollUp.classList.remove("show-scroll");
+}
 
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
 
-/*=============== DARK LIGHT THEME ===============*/ 
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+        const sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link');
+        } else {
+            sectionsClass.classList.remove('active-link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+/*=============== DARK LIGHT THEME ===============*/
